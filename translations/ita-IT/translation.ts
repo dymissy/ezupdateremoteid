b46c75eaf2868e4a4f1118d50b37b2e75f18e0cf
Simone D'Amico <simone.damico@yithemes.com>
2014-02-15 16:39:41 +0100

    Added ITA translation

---
+++ translations/ita-IT/translation.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1" language="ita_IT">
+<context>
+  <name>kernel/object</name>
+  <message>
+      <source>Update RemoteId</source>
+      <translation>Aggiorna RemoteId</translation>
+  </message>
+  <message>
+      <source>New RemoteId</source>
+      <translation>Nuovo RemoteId</translation>
+  </message>
+</context>
+</TS>
